add computation of score, and some minor fixes

aomip/XrayOperator.py
@@ -96,7 +96,8 @@ class XrayOperator(LinearOperator):
             np.ones(self.ndim) if vol_spacing is None else np.array(vol_spacing)
         )
         self.sino_spacing = (
-            np.ones(self.ndim) if sino_spacing is None else np.array(sino_spacing)
+            np.ones(self.ndim) if sino_spacing is None else np.array(
+                sino_spacing)
         )
         self.cor_offset = (
             np.zeros(self.ndim) if cor_offset is None else np.array(cor_offset)
@@ -126,7 +127,8 @@ class XrayOperator(LinearOperator):
                 f"Array containing principal point offset is of the wrong size (is {self.pp_offset.size}, expected {self.ndim - 1})"
             )
 
-        self.vol_descriptor = elsa.VolumeDescriptor(self.vol_shape, self.vol_spacing)
+        self.vol_descriptor = elsa.VolumeDescriptor(
+            self.vol_shape, self.vol_spacing)
         self.sino_descriptor = elsa.CircleTrajectoryGenerator.trajectoryFromAngles(
             thetas,
             self.vol_descriptor,
@@ -144,16 +146,19 @@ class XrayOperator(LinearOperator):
                     self.vol_descriptor, self.sino_descriptor
                 )
             else:
-                self.A = elsa.JosephsMethod(self.vol_descriptor, self.sino_descriptor)
+                self.A = elsa.JosephsMethod(
+                    self.vol_descriptor, self.sino_descriptor)
         elif projection_method == "siddons":
             if elsa.cudaProjectorsEnabled():
                 self.A = elsa.SiddonsMethodCUDA(
                     self.vol_descriptor, self.sino_descriptor
                 )
             else:
-                self.A = elsa.SiddonsMethod(self.vol_descriptor, self.sino_descriptor)
+                self.A = elsa.SiddonsMethod(
+                    self.vol_descriptor, self.sino_descriptor)
         else:
-            raise RuntimeError(f"Unknown projection method '{projection_method}'")
+            raise RuntimeError(
+                f"Unknown projection method '{projection_method}'")
 
         M = np.prod(sino_shape) * np.size(thetas)  # number of rows
         N = np.prod(vol_shape)  # number of columns

challenge/utils.py
@@ -2,6 +2,7 @@ from aomip import XrayOperator
 
 import numpy as np
 import scipy.io as spio
+from skimage.filters import threshold_otsu
 
 
 def loadmat(filename):
@@ -62,7 +63,7 @@ def load_htc2022data(filename, arc=360, arcstart=0, dtype="float32"):
     )
 
     sinogram = mat[dataset_name]["sinogram"].astype(dtype)
-    sinogram = prep_sinogram(sinogram)
+    sinogram = prep_sinogram(sinogram)[:, : (arc * 2)]
 
     return sinogram, A
 
@@ -72,3 +73,65 @@ def prep_sinogram(sino):
     sino = sino.transpose(1, 0)
     sino = np.flip(sino, axis=1)
     return sino
+
+
+def segment(img):
+    """Create segmented binary image using otsu thresholding"""
+    img_seg = img.copy()
+    img_seg[img_seg < 0] = 0
+    thresh = threshold_otsu(img_seg)
+    img_seg[img < thresh] = 0
+    img_seg[img > thresh] = 1
+
+    img_seg = img_seg.astype(bool)
+
+    return img_seg
+
+
+def calculate_score(recon, groundthruth):
+    """Compute score of the reconstruction comapred to the ground truth.
+
+    The score will be in the range of [-1, 1], where 1 (best) represents a
+    perfect reconstruction, 0 is equivalent to a random reconstruction,
+    and -1 indicates absolute disagreement.
+
+    The score is computed using morphological closing, a confusion matrix and
+    Matthews correlation coefficient. This is the code provided by the Helsinki
+    Tomography challenge to compute the score.
+
+    Parameters
+    ----------
+    recon : :obj:`np.ndarray`
+        Binary segmentation of the reconstruction to calculate the score to.
+    groundthruth : :obj:`np.ndarray`
+        Binary segmentation of the ground truth reconstruction
+    """
+
+    def AND(x, y):
+        return np.logical_and(x, y)
+
+    def NOT(x):
+        return np.logical_not(x)
+
+    # if the reconstruction does not have the correct size, score is 0
+    if recon.shape != (512, 512):
+        return 0
+
+    # confusion matrix with true positives, true negatives, false positives, and
+    # false negatives
+    TP = float(len(np.where(AND(groundthruth, recon))[0]))
+    TN = float(len(np.where(AND(NOT(groundthruth), NOT(recon)))[0]))
+    FP = float(len(np.where(AND(NOT(groundthruth), recon))[0]))
+    FN = float(len(np.where(AND(groundthruth, NOT(recon)))[0]))
+    cmat = np.array([[TP, FN], [FP, TN]])
+
+    # Matthews correlation coefficient (MCC)
+    numerator = TP * TN - FP * FN
+    denominator = np.sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))
+
+    if denominator == 0:
+        score = 0
+    else:
+        score = numerator / denominator
+
+    return score