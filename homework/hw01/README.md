bf2dfc3311298ee498ec3e9c6543cef4c5c9e1e5
Matilde T <ge83bug@tum.de>
2023-04-27 15:06:59 +0200

    finish flat-field correction

--- homework/hw01/README.md
+++ homework/hw01/README.md
@@ -5,3 +5,8 @@
 ### i) Flat-field correction
 
 I'll start by implementing this function as a simple function inside a file, maybe in the future it may be sensible to move it inside some sort of package?
+
+To test the functionality, I used some data from [this](https://zenodo.org/record/2686726) dataset. I selected a few frames just to give a brief demonstration. If I understand correctly the files `di000000.tif` and `io000000.tif` are the dark frame and flat field respectively. The results seems satisfying.
+
+![Flat field correction test](flatFieldCorrection.png "Flat field correction test")
+
--- homework/hw01/hw01.py
+++ homework/hw01/hw01.py
@@ -5,9 +5,30 @@ import os
 
 def test_flatFieldCorrection():
     cwd=os.getcwd()
-    image = mpimg.imread(cwd+"/homework/hw01/walnut/20201111_walnut_0701.tif")
-    plt.imshow(image)
+    dark_frame = mpimg.imread(cwd+"/homework/hw01/tubeV1/di000000.tif")
+    flat_field = mpimg.imread(cwd+"/homework/hw01/tubeV1/io000000.tif")
+    fig, ax = plt.subplots(5, 4, layout="compressed")
+    fig.suptitle("Flat field correction")
+    for i in range(10):
+        image = mpimg.imread(cwd+"/homework/hw01/tubeV1/scan_000"+str(i)+str(i)+"6.tif")
+        if i<5:
+            ax[i][0].imshow(image, cmap="gray")
+            ax[i][0].axis("off")
+            ax[i][1].imshow(flatFieldCorrection(image, dark_frame, flat_field), cmap="gray")
+            ax[i][1].axis("off")
+        else:
+            ax[i-5][2].imshow(image, cmap="gray")
+            ax[i-5][2].axis("off")
+            ax[i-5][3].imshow(flatFieldCorrection(image, dark_frame, flat_field), cmap="gray")
+            ax[i-5][3].axis("off")
+    ax[0][0].set_title("Original image")
+    ax[0][1].set_title("Corrected image")
+    ax[0][2].set_title("Original image")
+    ax[0][3].set_title("Corrected image")
+    plt.tight_layout()
+    plt.savefig(cwd+"/homework/hw01/flatFieldCorrection.png")
     plt.show()
+    
     
 
 if __name__ == "__main__":
