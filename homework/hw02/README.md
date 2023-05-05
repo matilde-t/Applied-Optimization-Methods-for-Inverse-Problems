0f22d4884a34ffc6796957b25c9b031ce28abf09
Matilde Tozzi <ge83bug@tum.de>
2023-05-06 00:37:23 +0200

    finish preprocessing again

--- homework/hw02/README.md
+++ homework/hw02/README.md
@@ -1,3 +1,15 @@
 # Homework 2
 
+For this homework I'm using the [Cone-Beam Computed Tomography Dataset of a Seashell](https://zenodo.org/record/6983008), that I saved on the available remote machine in `/srv/ceph/share-all/aomip/6983008_seashell`. Given that the documentation says that the image should be moved 4px to the left for best results, I perform this operation when opening the files.
+
+![Sinograms of different rows](seashell.png "Sinograms of different rows")
+
+I show just a small sample out of the dataset, with 4px correction and binning applied 3 times.
+
 ## Part 1: Preprocessing again
+
+I implemented this function in the file `Slicing.py`. It takes a collection of projections and extracts the requested line, stacking each line vertically in order to create a sinogram.
+
+I tried with different rows and the results seem satisfying.
+
+![Sinograms of different rows](sinogram.png "Sinograms of different rows")
--- homework/hw02/hw02.py
+++ homework/hw02/hw02.py
@@ -1,21 +1,38 @@
 import aomip
-import numpy as np
 import matplotlib.pyplot as plt
-import os 
 import tifffile
+import os
 
 def test_Slicing():
-    cwd = os.getcwd()
-    path = cwd + '/homework/hw02/sea/'
+    path = '/srv/ceph/share-all/aomip/6983008_seashell'
     img = []
     for image in os.listdir(path):
-        tmp = aomip.centerOfRotationCorrection(tifffile.imread(path + image), -4, 1) # correction as suggested in the dataset
+        tmp = aomip.centerOfRotationCorrection(tifffile.imread(os.path.join(path, image)), -4, 1) # correction as suggested in the dataset
         img.append(tmp)
-    sino = aomip.slice(img, 5)
-    plt.imshow(sino, cmap='gray')
-    plt.show()
-    
+    sino = []
+    idx = []
+    for i in range(0,2240,500):
+        sino.append(aomip.slice(img, i))
+        idx.append(i)
+    n = len(sino)
+    fig, ax = plt.subplots(1,n-1)
+    for i in range(1,n):
+        ax[i-1].imshow(sino[i], cmap='gray')
+        ax[i-1].set_title('Row {}'.format(idx[i]))
+    fig.suptitle('Sinograms of different rows')
+    plt.tight_layout()
+    plt.savefig('./homework/hw02/sinogram.png')
+    return
+
+def printShell():
+    img = tifffile.imread('/srv/ceph/share-all/aomip/6983008_seashell/20211124_seashell_0666.tif')
+    img = aomip.centerOfRotationCorrection(img, -4, 1)
+    img = aomip.binning(img, 3)
+    plt.imsave('./homework/hw02/seashell.png', img, cmap='gray')
+    return
+
     
 
 if __name__ == '__main__':
-    test_Slicing()
+    printShell()
+    # test_Slicing()
